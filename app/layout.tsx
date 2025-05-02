88b0751ea8517932403d3fe49dd239c786ea1e46
Martin Boynton <mistergoomba@gmail.com>
2025-05-01 19:05:42 -0700

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,36 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  metadataBase: new URL('https://meowtin.com'),
+  title: "Meowtin's Domain",
+  description:
+    "Welcome to Meowtin's Domain, the home page and personal portfolio of Martin Boynton...",
+  themeColor: '#000000',
+  openGraph: {
+    title: "Meowtin's Domain",
+    description:
+      "Welcome to Meowtin's Domain, the home page and personal portfolio of Martin Boynton...",
+    url: 'https://meowtin.com',
+    siteName: "Meowtin's Domain",
+    images: [
+      {
+        url: 'https://meowtin.com/share-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: "Meowtin's Domain",
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Meowtin's Domain",
+    description:
+      "Welcome to Meowtin's Domain, the home page and personal portfolio of Martin Boynton...",
+    images: ['https://meowtin.com/share-image.jpg'],
+  },
+};
 
 export default function RootLayout({
   children,
